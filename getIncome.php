90bc83d9d7af74d4e44475270e60a9c7c671b787
Jacob19 <cfjacob026@gmail.com>
2016-08-06 14:53:46 +0100

    adding income calculations

--- getIncome.php
+++ getIncome.php
@@ -2,7 +2,7 @@
 
 require_once ('readDataFromCsv.php');
 
-$values = array();
+$low_income = array();
 $income = array();
 
 $last = end(array_keys($arr));
@@ -13,7 +13,25 @@ echo "<pre>";
 
 for ($i=1; $i < $last; $i++) { 
 	
-	$income[] = ($arr[$i][14]);
+	$income[] = $arr[$i][14];
+	if ($arr[$i][14] > 50000 && $arr[$i][14]<= 60000) {
+		$low_income[$i]['Age'] 							= $arr[$i][0];
+		$low_income[$i]['Work Class'] 			= $arr[$i][1];									
+		$low_income[$i]['Aadhar'] 					= $arr[$i][2];
+		$low_income[$i]['Education'] 				= $arr[$i][3];
+		$low_income[$i]['Education Number'] = $arr[$i][4];
+		$low_income[$i]['Marital Status'] 	= $arr[$i][5];
+		$low_income[$i]['Occupation'] 			= $arr[$i][6];
+		$low_income[$i]['Relationship'] 		= $arr[$i][7];
+		$low_income[$i]['Race'] 						= $arr[$i][8];
+		$low_income[$i]['Sex'] 							= $arr[$i][9];
+		$low_income[$i]['Capital Gain'] 		= $arr[$i][10];
+		$low_income[$i]['Capital Loss'] 		= $arr[$i][11];
+		$low_income[$i]['Hr/Week'] 					= $arr[$i][12];
+		$low_income[$i]['Country'] 					= $arr[$i][13];
+		$low_income[$i]['Income'] 					= $arr[$i][14];
+	}
 }
 
-print_r($income);
+// print_r($low_income);
+print_r(json_encode($low_income));
