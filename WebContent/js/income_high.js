83e894241721e6f9241f36ebefd1fa9928a50a87
Uday Kumar <Uday Kumar>
2016-08-07 08:29:46 +0530

    Bootstrap related changes and indentation

--- WebContent/js/income_high.js
+++ WebContent/js/income_high.js
@@ -186,7 +186,7 @@ function loadDoc2(obj) {
              }
              });*/
 
-            $('#container-fluid').highcharts({
+            $('#census-chart').highcharts({
                 chart: {
                     type: 'column'
                 },
